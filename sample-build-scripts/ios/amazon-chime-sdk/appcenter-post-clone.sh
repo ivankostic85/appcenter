2414249543c10e8a3cf6abff610ecca7579d67a0
Ivan Kostic <v-ivankostic@microsoft.com>
2021-01-19 09:51:33 +0100

    Amazon Chime SDK download script

---
+++ sample-build-scripts/ios/amazon-chime-sdk/appcenter-post-clone.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+#
+# Downloads Amazon Chime SDK Media binaries
+
+# fail if any command fails
+set -e
+# debug log
+set -x
+
+echo "Start dowloading AmazonChimeSDKMedia-0.9.0.tar.gz"
+curl -O https://amazon-chime-sdk-ios.s3.amazonaws.com/media/0.9.0/AmazonChimeSDKMedia-0.9.0.tar.gz
+
+echo "Finished dowloading. Extracting AmazonChimeSDKMedia-0.9.0.tar.gz"
+tar -xzvf AmazonChimeSDKMedia-0.9.0.tar.gz
+
+echo "Extracting finished. Cleaning up..."
+rm -rf AmazonChimeSDKMedia-0.9.0.tar.gz
